6761d79beed8aaffac27c29d8dafa323956b7cea
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-05-20 11:49:48 -0700

    Generate migrations file to add UserProfile

--- src/PetAdoptionCRM/Migrations/20170520184917_AddUserProfiles.Designer.cs
+++ src/PetAdoptionCRM/Migrations/20170520184917_AddUserProfiles.Designer.cs
@@ -8,9 +8,10 @@ using PetAdoptionCRM.Models;
 namespace PetAdoptionCRM.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20170520184917_AddUserProfiles")]
+    partial class AddUserProfiles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
