d72019650682b8766e72e037f1cc4d6b1476e77d
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-05-20 10:35:28 -0700

    Update applicationcontext class with appropriate constructors

--- src/PetAdoptionCRM/Models/ApplicationContext.cs
+++ src/PetAdoptionCRM/Models/ApplicationContext.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace PetAdoptionCRM.Models
 {
-    public class ApplicationContext
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationContext()
+        {
+
+        }
+
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=tcp:petadoptioncrm.database.windows.net,1433;Initial Catalog=PetAdoptionCRM_db;Persist Security Info=False;User ID=AllieH;Password=Thispw12!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
