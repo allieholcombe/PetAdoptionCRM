e64f0a8af2b7f8ed1584d30906aef03d1b22999e
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-05-20 10:33:22 -0700

    Update applicatiounuser class with appripriate using statements and cast as identityuser

--- src/PetAdoptionCRM/Models/ApplicationUser.cs
+++ src/PetAdoptionCRM/Models/ApplicationUser.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PetAdoptionCRM.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
     }
 }
