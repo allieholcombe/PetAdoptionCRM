03af6acf28eb9859bfabf70916e895df836d85c6
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-05-22 17:20:29 -0700

    Add Jquery UI bower package

--- src/PetAdoptionCRM/wwwroot/js/site.js
+++ src/PetAdoptionCRM/wwwroot/js/site.js
@@ -7,33 +7,36 @@
         var url = 'PopulateBreedList';
         var dropdown = $("#breed-selector");
         var speciesVal = $("#species-selector option:selected").val();
-        dropdown.empty();
-        //console.log(speciesVal);
-        //console.log(typeof speciesVal);
-        $.ajax({
-            url: 'PopulateBreedList',
-            data: { speciesVal: speciesVal },
-            type: "POST",
-            datatype: "json",
-            error: function (xhr, status, error) {
-                console.log("borked");
-                console.log(error);
-            },
-            success: function (json) {
-                $(json).each(function () {
-                    //debugger;
-                    $(document.createElement('option'))
-                                .attr('value', this.id)
-                                .text(this.name)
-                                .appendTo(dropdown);
-                })
+        if (speciesVal === 3 || speciesVal === 4) {
 
-            },
-            complete: function () {
-                $("#breed-form-div").show();
+            dropdown.empty();
+            //console.log(speciesVal);
+            //console.log(typeof speciesVal);
+            $.ajax({
+                url: 'PopulateBreedList',
+                data: { speciesVal: speciesVal },
+                type: "POST",
+                datatype: "json",
+                error: function (xhr, status, error) {
+                    console.log("borked");
+                    console.log(error);
+                },
+                success: function (json) {
+                    $(json).each(function () {
+                        //debugger;
+                        $(document.createElement('option'))
+                                    .attr('value', this.id)
+                                    .text(this.name)
+                                    .appendTo(dropdown);
+                    })
 
-            }
-        })
+                },
+                complete: function () {
+                    $("#breed-form-div").show();
+
+                }
+            })
+        }
     }
 
 
